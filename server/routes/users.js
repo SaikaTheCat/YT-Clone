4b4f156f5353aba1867421790122c839698b864f
Se Joon Oh <see.joon@inateltech.com>
2023-11-08 18:27:01 -0600

    first commit

---
+++ server/routes/users.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { deleteUser, dislike, getUser, like, subscribe, unsubscribe, update } from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
+
+const router = express.Router();
+
+//update user
+router.put("/:id", verifyToken, update)
+//delete user
+router.delete("/:id", deleteUser)
+//get a user
+router.get("/find/:id", getUser)
+//subs
+router.put("/sub/:id", subscribe)
+//unsub
+router.put("/unsub/:id", unsubscribe)
+//like
+router.put("/like/:videoId", like)
+//dislike
+router.put("/dislike/:videoId", dislike)
+
+export default router;
